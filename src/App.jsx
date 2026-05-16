b9aa681278660acb7d975637e506b0588bc4ff5d
Eugenia <eugeniafer2005@gmail.com>
2026-05-16 16:56:33 -0300

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,130 +1,9 @@
-/*import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
-}
-export default App */
-import { useState } from "react";
+import { useState } from "react"; //hook de React
 import "./App.css";
 
 function App() {
 
-  const tips = [
+  const tips = [ //Array de tips
     "Tomar descansos cortos",
     "Trabajar en bloques de tiempo",
     "Dormir bien para rendir mejor",
@@ -134,28 +13,26 @@ function App() {
     "Priorizar tareas importantes"
   ];
 
-  const [selected, setSelected] = useState(0);
+  const [selected, setSelected] = useState(0); //useState, estado del tip actual
 
-  const [votos, setVotos] = useState([
+  const [votos, setVotos] = useState([ //useState, estado de los votos
     0, 0, 0, 0, 0, 0, 0
   ]);
 
-  // tips ya vistos
-  const [vistos, setVistos] = useState([0]);
+  const [vistos, setVistos] = useState([0]); // useState, tips ya vistos
 
-  // votar
+  //Función para votar
   const votar = () => {
     const nuevos = [...votos];
     nuevos[selected]++;
-    setVotos(nuevos);
+    setVotos(nuevos); //Actualización del estado
   };
 
-  // cambiar tip sin repetir
-  const otroTip = () => {
+  const otroTip = () => { // Función para cambiar tip sin repetir
 
     let nuevosVistos = vistos;
 
-    // si ya mostró todos → reinicia
+    // Si ya mostró todos, reinicia
     if (vistos.length === tips.length) {
       nuevosVistos = [selected];
     }
@@ -166,17 +43,17 @@ function App() {
       random = Math.floor(Math.random() * tips.length);
     } while (nuevosVistos.includes(random));
 
-    setSelected(random);
+    setSelected(random); // Actualización de estados
     setVistos([...nuevosVistos, random]);
   };
 
-  // reiniciar votos
+  // Función para reiniciar votos
   const reiniciar = () => {
     setVotos([0, 0, 0, 0, 0, 0, 0]);
     setVistos([selected]);
   };
 
-  // buscar el más votado
+  // Buscar el más votado
   let indexMax = 0;
 
   if (votos[1] > votos[indexMax]) indexMax = 1;
@@ -210,7 +87,8 @@ function App() {
 
           <div className="buttons">
 
-            <button
+            {/* Eventos onClick */}
+            <button 
               className="vote-btn"
               onClick={votar}
             >
